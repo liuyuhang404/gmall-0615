d11652309d1840acbfbd5c29295dec659119d341
刘宇航 <www.272641237@qq.com>
2019-10-29 00:22:39 +0800

    第一次初始化

---
+++ gnall-ums/src/main/java/com/atguigu/gnall/ums/GnallUmsApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gnall.ums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GnallUmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GnallUmsApplication.class, args);
+	}
+
+}
---
+++ gnall-ums/src/test/java/com/atguigu/gnall/ums/GnallUmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gnall.ums;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GnallUmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
